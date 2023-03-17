ac3c8f9d1cb0efda2131de354b30735135c8af57
BuildTools <unconfigured@null.spigotmc.org>
2023-03-16 19:51:49 -0500

    Added new pages

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/home')
+@app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/reginfo')
+def reginfo():
+    return render_template('regulation_info.html')
+
+@app.route('/moreinfo')
+def moreinfo():
+    return render_template('more_info.html')
+
 
 
